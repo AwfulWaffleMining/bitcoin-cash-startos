ad161a264299f2f27d3021aee97e5a5bb33344e9
AwfulWaffleMining <awfulwafflemining@gmail.com>
2026-05-23 04:53:00 -0500

    fix: startos/init/index.ts

--- startos/init/index.ts
+++ startos/init/index.ts
@@ -1,6 +1,16 @@
 import { sdk } from '../sdk'
+import { setDependencies } from '../dependencies'
+import { setInterfaces } from '../interfaces'
+import { versionGraph } from '../versions'
+import { actions } from '../actions'
+import { restoreInit } from '../backups'
 
-export const { init, uninit } = sdk.setupInit({
-  init: async ({ effects }) => {},
-  uninit: async ({ effects }) => {},
-})
+export const init = sdk.setupInit(
+  restoreInit,
+  versionGraph,
+  setInterfaces,
+  setDependencies,
+  actions,
+)
+
+export const uninit = sdk.setupUninit(versionGraph)
