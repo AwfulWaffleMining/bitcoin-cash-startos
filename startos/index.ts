b4e0a426c9116a98dddac0768d01056d8995aaa5
AwfulWaffleMining <awfulwafflemining@gmail.com>
2026-05-23 04:52:59 -0500

    fix: startos/index.ts

--- startos/index.ts
+++ startos/index.ts
@@ -1,7 +1,11 @@
+/**
+ * Plumbing. DO NOT EDIT.
+ */
 export { createBackup, restoreInit } from './backups'
 export { main } from './main'
 export { init, uninit } from './init'
 export { actions } from './actions'
 import { buildManifest } from '@start9labs/start-sdk'
-import { manifest as sdkManifest } from './manifest/index'
-export const manifest = buildManifest([[sdkManifest.version, sdkManifest]], sdkManifest)
+import { manifest as sdkManifest } from './manifest'
+import { versionGraph } from './versions'
+export const manifest = buildManifest(versionGraph, sdkManifest)
